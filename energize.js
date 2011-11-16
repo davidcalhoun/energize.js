d94726b0e34d8ee7bccc7d1a1f2fb201e43f6995
davidcalhoun <david.b.calhoun@gmail.com>
2011-11-15 21:35:20 -0800

    complete revision

--- energize.js
+++ energize.js
@@ -1,120 +1,68 @@
-/* energize.js - a tiny JavaScript library to make links snappy in iOS */
-var energize = (function(){
-  var MIN_MS_DELAY = 0, // todo
-      MAX_Y_PX = 10,
-      energizedNodes = [];
- 
-  /* Only target Apple touch-based browsers */
-  var energizable = ('ontouchstart' in window && navigator.vendor.match(/apple/i));
+// energize.js: speed up click events on mobile devices
+(function() {
+  // don't add to non-touch devices
+  if(!('ontouchstart' in window)) return;
   
-  /* convenience addEventListener for lazy people (me) */
-  var on = function(elt, type, fn){
-    elt.addEventListener(type, fn, false);
+  var isThresholdReached = function(startXY, xy) {
+    /*
+        Determine if the scroll threshold has been reached
+
+        Examples:
+        isThresholdReached([0,0], [5,5]);    // false
+        isThresholdReached([0,0], [20,20]);  // true
+    */
+    return Math.abs(startXY[0] - xy[0]) > 10 || Math.abs(startXY[1] - xy[1]) > 10;
   };
-  
-  if(!energizable) {
-    // short-circuit it!  ('Input... need more input!')
-    return {
-      clk: function(elt, fn){
-        on(elt, 'click', fn);
-      },
-      
-      delegateClk: function(elt, tag, fn){
-        on(elt, 'click', function(e){
-          if(e.target.nodeName !== tag.toUpperCase()) return;
-          fn(e);
-        });
-      }
+  var touchstart = function(e) {
+    // setup the initial values
+    this.startXY = [e.touches[0].clientX, e.touches[0].clientY];
+    this.treshold = false;
+  };
+  var touchmove = function(e) {
+    // check if the user is scrolling past the threshold
+    if(this.treshold) return false;  // noop if the threshold has already been reached
+    this.threshold = isThresholdReached(this.startXY, [e.touches[0].clientX, e.touches[0].clientY]);
+  };
+  var touchend = function(e) {
+    // don't fire a click if the user scrolled past the threshold
+    if(this.treshold || isThresholdReached(this.startXY, [e.changedTouches[0].clientX, e.changedTouches[0].clientY])) {
+      return;
     }
-  }
-  
-  var energize = function(elt){
-    var focusedElt, startTime, startXY = [];
     
-    // don't needlessly energize it twice
-    if(energizedNodes.indexOf(elt) !== -1) return;
-    
-    // keep track of energized nodes
-    energizedNodes.push(elt);
-    
-    // touchstart event
-    on(elt, 'touchstart', function(e){
-      var touch = e.touches[0];
-
-      focusedElt = touch.target;
-      startTime = Date.now();
-      startXY = [touch.screenX, touch.screenY];
-    });
-
-    // touchend event
-    on(elt, 'touchend', function(e){
-      var evt,
-          touch = e.changedTouches[0],
-          shouldFireClick;
-      
-      // make sure the targets match up, the MAX_Y_PX is in an acceptable range, and MIN_MS_DELAY is in an acceptable range
-      shouldFireClick = (focusedElt && touch.target === focusedElt && Math.abs(startXY[1]) - touch.screenY < MAX_Y_PX && Date.now() - startTime > MIN_MS_DELAY);
-      
-      // short-circuit
-      if(!shouldFireClick) {
-        focusedElt = null;
-        return;
-      }
-      
-      // create and fire a click event
-			// https://developer.mozilla.org/en/DOM/event.initMouseEvent
-      evt = document.createEvent('MouseEvents');
-      evt.initMouseEvent('click', true, true, window, 0, touch.screenX, touch.screenY, touch.clientX, touch.clientY, false, false, false, false, 0, null);
-      evt.simulated = true;   // distinguish this from a nonsimulated click
-      focusedElt.dispatchEvent(evt);
-      focusedElt = null;
-    });
-  }
-  
-  /*
-      click
-      Speed up click events on the given elt.
-      
-      Example:
-      energize.clk(document.getElementById('test'), function(e){
-        // code here
-      });
-  */
-  var click = function(elt, fn){
-    
-    
-    // energize the element, attaching touchstart and touchend handlers
-    energize(elt);
-    
-    // click
-    on(elt, 'click', function(e){
-      if(!e.simulated) return;  // don't run nonsimulated (nonenergized) clicks
-      fn(e);
-    });
-  }
-  
-  /*
-      delegateClick
-      Speed up click events on all descendants of elt of the given tag type.
-      
-      Example:
-      energize.delegateClick(document.getElementById('container'), 'a', function(e){
-        // code here
-      });
-  */
-  var delegateClick = function(elt, tag, fn){
-    energize(elt);
-    
-    on(elt, 'click', function(e){
-      // don't fire on nonsimulated events and nonmatching elements
-      if(!e.simulated || e.target.nodeName !== tag.toUpperCase()) return;
-      
-      fn(e);
-    });
-  }
-  
-  return {
-    clk: click,
-    delegateClk: delegateClick
+    // create and fire a click event on the target element
+    // https://developer.mozilla.org/en/DOM/event.initMouseEvent
+    var touch = e.changedTouches[0],
+        evt = document.createEvent('MouseEvents');
+    evt.initMouseEvent('click', true, true, window, 0, touch.screenX, touch.screenY, touch.clientX, touch.clientY, false, false, false, false, 0, null);
+    evt.simulated = true;   // distinguish this from a nonsimulated click
+    e.target.dispatchEvent(evt);
   };
+  var click = function(e) {
+    /*
+        Prevent ghost clicks by only allowing clicks we created
+        in the click event we fired (look for e.simulated)
+    */
+    if(!e.simulated || (window.navigator.standalone && e.target.nodeName === 'A' && e.target.getAttribute("href"))) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+    
+    /* 
+       Special logic for standalone web apps
+       See http://stackoverflow.com/questions/2898740/iphone-safari-web-app-opens-links-in-new-window
+    */
+    if(window.navigator.standalone && e.target.nodeName === 'A' && e.target.getAttribute("href")) {
+      window.location=e.target.getAttribute("href");
+    }
+  };
+
+  /*
+      All the events we care about bubble up to document,
+      so we can handle them there instead of 
+  */
+  document.addEventListener('touchstart', touchstart, false);
+  document.addEventListener('touchmove', touchmove, false);
+  document.addEventListener('touchend', touchend, false);
+  document.addEventListener('click', click, true);
+  
 })();
